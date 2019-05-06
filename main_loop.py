9782649a7ac5fc95ff0fce4e5ce6b8b12403322e
Petukhov Dmitry <petumit@yandex.ru>
2019-05-06 20:52:40 +0300

    Fixed issue with threads, added timers

--- main_loop.py
+++ main_loop.py
@@ -24,11 +24,15 @@ Appends list containing response, device name and device number to list result(a
 
 def obtain_responses():
     """Obtains responses from devices. Uses function 'request_permission' """
+    threads = list()
     for i in range(len(devices)):
         random_time = 0.01 * random.random()
         t = threading.Timer(random_time, request_permission, [i])
         t.start()
-    time.sleep(1)  # for asynchronous generation and obtaining
+        time.sleep(0.1)  # delay so you can see what's happening
+        threads.append(t)
+    for thread in threads:
+        thread.join()  # for asynchronous generation and obtaining
     print(result)
 
 
@@ -43,17 +47,26 @@ def handle_responses():
 
 
 try:
-    while 1:
+    for i in range(10):
+        print('TEST NO {n}'.format(n=i+1))
         obtain_responses()
         print('\n[Beginning of exchange cycle]\n')
+        elapsed_time = 0  # pseudo time
+        elapsed_time += len(devices)*20*10**-5  # time to obtain data from N devices
+        elapsed_time += 2*10**-4  # switch receives data
+        elapsed_time += 5*10**-4  # switch processes data
+        elapsed_time += 2*10**-4  # switch sends data
         while len(result) > 0:
             data = handle_responses()
-
             receiver = data[1]
             packet = data[2]
 
-            print('Packet :' + str(packet) + ' has been received by ' + receiver.device_name)
+            print("""Packet : {packet} has been received by {receiver},
+             [elapsed time: {elapsed_time} sec.]"""
+            .format(packet=str(packet), receiver=receiver.device_name,
+            elapsed_time=elapsed_time))
             print()
+            elapsed_time += 2*10**-4
         print('[End of exchange cycle]\n')
 
 except KeyboardInterrupt:
